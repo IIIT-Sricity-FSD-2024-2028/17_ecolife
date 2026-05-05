555d016d33ea3563d1b71ab7069d7e1499ad00c6
Mallela_harish_3558 <harish.m24@iiits.in>
2026-05-05 09:23:29 +0530

    Delete back-end/dist/auth directory

--- back-end/dist/auth/auth.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { LoginDto } from '../common/base.dto';
-import { InMemoryStoreService } from '../common/in-memory-store.service';
-export declare class AuthService {
-    private readonly store;
-    constructor(store: InMemoryStoreService);
-    login(dto: LoginDto): import("../in-memory/entities").User;
-}
