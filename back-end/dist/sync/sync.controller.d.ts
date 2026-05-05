396ca2ca9fb578245d8d9f19db55bb38f2897c90
Mallela_harish_3558 <harish.m24@iiits.in>
2026-05-05 09:36:55 +0530

    Delete back-end/dist/sync directory

--- back-end/dist/sync/sync.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { SnapshotDto } from '../common/base.dto';
-import { RorizonDb } from '../in-memory/entities';
-import { SyncService } from './sync.service';
-export declare class SyncController {
-    private readonly syncService;
-    constructor(syncService: SyncService);
-    getSnapshot(): import("../common/crud.types").ApiResponse<RorizonDb>;
-    replaceSnapshot(body: SnapshotDto): import("../common/crud.types").ApiResponse<RorizonDb>;
-}
