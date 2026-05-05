e7cea3dd424ff36c397e5fe1dc60acf217b33565
Mallela_harish_3558 <harish.m24@iiits.in>
2026-05-05 09:41:57 +0530

    Delete back-end/dist/swagger-file.js

--- back-end/dist/swagger-file.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const main_1 = require("./main");
-async function writeSwagger() {
-    const app = await (0, main_1.createApp)();
-    await app.close();
-}
-void writeSwagger();
-//# sourceMappingURL=swagger-file.js.map
